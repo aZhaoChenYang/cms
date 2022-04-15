e606ede6d8832bcc563820f850b42bc0dda7fd14
赵晨阳 <zhaochenyang10@huawei.com>
2022-04-15 17:04:04 +0800

    Changes

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
    // const Api_url = 'http://rhskl.ruhuashop.com/'
-	 const Api_url = 'http://127.0.0.1:8000/api/v1.0/'
+const Api_url = 'http://127.0.0.1:8000/api/v1.0/'
 export {Api_url}
